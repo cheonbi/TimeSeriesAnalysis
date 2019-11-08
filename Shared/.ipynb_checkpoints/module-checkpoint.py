db94f993dcad48fa1b1e5d6c203d450385ed2e18
cheonbi <cheonbi@snu.ac.kr>
2019-11-09 00:50:48 +0900

    no message

--- Shared/.ipynb_checkpoints/module-checkpoint.py
+++ Shared/.ipynb_checkpoints/module-checkpoint.py
@@ -1,24 +1,13 @@
 # Install packages from jupyter Notebook
 # !python -m pip install --user --upgrade pip
-# !pip install pandas-datareader
 # !pip install tqdm
-# !pip install missingno
 # !conda config --add channels conda-forge
 # !conda install --yes hmmlearn
 # !pip install hmmlearn
-# !pip install xgboost
-# !pip install lightgbm
-# !pip install arch
-# !pip install pyramid-arima
 
 # Auto reload
 # %reload_ext autoreload
 # %autoreload 2
-
-# Ignore the warnings
-import warnings
-warnings.filterwarnings('always')
-warnings.filterwarnings('ignore')
 
 # System related and data input controls
 import os
@@ -26,6 +15,16 @@ import glob
 from urllib.request import urlopen
 from io import BytesIO
 from zipfile import ZipFile
+os.system('pip install pandas-datareader')
+os.system('pip install missingno')
+os.system('pip install xgboost')
+os.system('pip install lightgbm')
+os.system('pip install arch')
+
+# Ignore the warnings
+import warnings
+warnings.filterwarnings('always')
+warnings.filterwarnings('ignore')
 
 # datasets
 import pandas_datareader.data as web
